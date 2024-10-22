2555ecf9b5ed0071f62958b58a43c98b63125514
Gavin Clarke <clarke.gavin@gmail.com>
2024-10-22 21:39:55 +0100

    restructuring code for re-usability

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -13,48 +13,57 @@ import seaborn as sns
 # read dataset into a DataFrame
 df = pd.read_csv("datasets/assessment_cluster_dataset.csv")
 
-
 def describe_data_out(dfs, path, separator):
     dfs.describe().to_csv(path_or_buf=path, sep=separator)
 
 
-print("Dataset Information: ")
-df.info()
+def describe_data(dfs):
+    print("Data Info:")
+    dfs.info()
+    print(f"Data Description: \n {dfs.describe()}")
 
+
+def null_check(dfs):
+    print(f"Null Data Check: \n {dfs.isnull().sum()}")
+
+
+def duplicate_check(dfs):
+    print(f"Duplicate Data Check: \n {dfs[dfs.duplicated()]}")
+
+
+def histogram(dfs):
+    print("Histogram Plot")
+    df.hist(figsize=(10, 7))
+    plt.show()
+
+
+def scatter_3d(dfs, x, y, z, title='3D Scatter Plot'):
+    print(title)
+    fig = px.scatter_3d(dfs, x=x, y=y, z=z)
+    fig.update_layout(title=dict(text=title))
+    plotly.offline.plot(fig)
+
+
+# unscaled data
 describe_data_out(df, "datasets/dataset_description.csv", ',')
-print("Dataset Description: ")
-print(df.describe())
+describe_data(df)
+null_check(df)
+duplicate_check(df)
+histogram(df)
+scatter_3d(df, 'att1', 'att2', 'att3', '3D distribution of unscaled data')
 
-# check if any attribute values are null
-print("NULL Check")
-print(df.isnull().sum())
-
-# Duplicate check
-print("Duplicate Check")
-print(df[df.duplicated()])
-
-# Histogram for frequency distribution
-df.hist(figsize=(10, 7))
-#plt.show()
-
-print("3D Scatter Plot")
-fig = px.scatter_3d(df, x='att1', y='att2', z='att3')
-fig.update_layout(title=dict(text="3D distribution of unscaled data"))
-#plotly.offline.plot(fig)
-
-
-# scale data into a new dataframe retaining original column names
+# scaled data
 scaled_df = StandardScaler().fit_transform(df)
 scaled_df = pd.DataFrame(scaled_df, columns=df.columns)
-print("Scaled data--------------------")
-print("Scaled Data Histogram")
-scaled_df.hist(figsize=(10, 7))
-#plt.show()
+describe_data_out(scaled_df, "datasets/dataset_description.csv", ',')
+describe_data(scaled_df)
+null_check(scaled_df)
+duplicate_check(scaled_df)
+histogram(scaled_df)
+scatter_3d(scaled_df, 'att1', 'att2', 'att3', '3D distribution of scaled data')
 
-print("3D Scatter Plot")
-fig = px.scatter_3d(scaled_df, x='att1', y='att2', z='att3')
-fig.update_layout(title=dict(text="3D distribution of scaled data"))
-#plotly.offline.plot(fig)
+"""
+
 
 
 print("Elbow Method")
@@ -94,7 +103,7 @@ print(centroid_table.head())
 fig = px.parallel_coordinates(centroid_table, 'cluster')
 plt.show()
 
-"""
+
 # Create k-means instance on scaled dataset
 kmeans = KMeans(n_clusters=4)
 results = kmeans.fit_predict(scaled_df[['att1', 'att2', 'att3']])
