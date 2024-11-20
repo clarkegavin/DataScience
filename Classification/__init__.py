9dcbdf72494f33472c56bfa6452b13be92857dca
Gavin Clarke <clarke.gavin@gmail.com>
2024-11-20 21:10:55 +0000

    added function to allow DataFrame.info() to be output to a csv file

--- Classification/__init__.py
+++ Classification/__init__.py
@@ -1,5 +1,11 @@
-
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
 import data as data
+import adsa_utils as ad
+import pandas as pd
+
 
 df_adult = data.read_data(file_location="datasets/assesment_adult_dataset.csv", sep=';')
 df_student = data.read_data(file_location="datasets/assessment_student_dataset.csv", remove_quotes=True, sep=';\s*')
@@ -12,7 +18,8 @@ data.print_data_statistics(df_student)
 data.print_data_statistics(df_adult)
 data.data_out(df_adult.describe(), "dataout/adult_describe.csv", ",")
 data.data_out(df_student.describe(), "dataout/student_describe.csv", ",")
-
+data.data_out(data.get_data_info(df_adult), "dataout/adult_info.csv")
+data.data_out(data.get_data_info(df_student), "dataout/student_info.csv")
 
 # scale continuous data
 data.scale_continuous_data(df_student)
@@ -24,3 +31,38 @@ data.data_out(df_student.describe(), "dataout/student_describe_scaled.csv", ",")
 
 
 # validate
+
+#
+# # create decision tree from adult dataset
+# print(df_adult.head())
+# # separate the class label
+# y = df_adult['label']
+# X = df_adult[df_adult.columns.difference(['labels'])]
+#
+# # split the dataset into training & test sets
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=43
+# )
+#
+# # encode categorical values for use with sklearn
+# X_train, encoders = data.encode_categorical_data_le(X_train)
+# # check: are there any id columns in the dataset that we need to get rid of?  e.g. education number?
+#
+# print(X_train.head())
+# features = list(X_train.columns)
+# print(X_train.head())
+# print(y_train.head())
+#
+# # baseline (default hyperparameters with exception of random_state for deterministic output
+# dtree = DecisionTreeClassifier(random_state=43)
+# dtree = dtree.fit(X_train, y_train)
+#
+# # cross validation - see adsa utils for custom_crossvalidation
+# ad.custom_crossvalidation(X_train, y_train, dtree)
+#
+#
+# plot_tree(dtree, filled=True, feature_names=features, class_names=dtree.classes_)
+# plt.figure(figsize=(25, 16))
+# plt.show()
+
+# XGBoost for categorical data
--- Classification/data.py
+++ Classification/data.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import csv
-
-from sklearn.preprocessing import StandardScaler
+import logging
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from ExceptionHandling import CustomFileNotFoundError
 
 
 def read_data(file_location, remove_quotes=False, sep=','):
     """
         custom function to read data from flat files, removing quotation qualifiers if necessary
     """
-    if remove_quotes:
-        df = pd.read_csv(file_location, sep=sep, skipinitialspace=True, quoting=csv.QUOTE_ALL, engine='python')
-    else:
-        df = pd.read_csv(file_location, sep=';')
-
-    return df
+    try:
+        if remove_quotes:
+            df = pd.read_csv(file_location, sep=sep, skipinitialspace=True, quoting=csv.QUOTE_ALL, engine='python')
+        else:
+            df = pd.read_csv(file_location, sep=';')
+        return df
+    except FileNotFoundError:
+        raise CustomFileNotFoundError(file_name=file_location)
 
 
 def clean_data(df):
@@ -27,6 +30,16 @@ def print_data_statistics(df):
     """ Custom function to print dataframe statistics to console"""
     df.info()
     print(df.describe())
+
+
+def get_data_info(df):
+    """
+    Custom function that returns a DataFrame containing DataFrame.info() so that it can be output to a file
+    :param df:
+    :return:
+    """
+    return pd.DataFrame({"name": df.columns, "non-nulls": len(df) - df.isnull().sum().values,
+                         "nulls": df.isnull().sum().values, "type": df.dtypes.values})
 
 
 def data_out(data, path, separator=','):
@@ -44,7 +57,19 @@ def scale_continuous_data(df):
     df[numeric_cols] = scaled_values
     return df
 
-def encode_categorical_data(df):
-    # TODO: Complete this function
-    """ Custom function to encode categorical data"""
+
+def encode_categorical_data_le(df):
+    """ Custom function to encode categorical data and return the encoded dataframe and a dictonary
+        with the original mappings
+    """
+    label_encoders = {}
+    for column in df.select_dtypes(include=['object', 'category']).columns:
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        label_encoders[column] = le  # Store encoders if need to reverse transformation
+
+    return df, label_encoders
+
+
+def encode_categorical_data_ohe(df):
     pass
